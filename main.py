c7328d101ad88e66aedc50db0b1d14e259b73e72
Onur Surmeli <onatsurmeli@gmail.com>
2023-05-26 19:49:10 +0200

    added indentation check and semicolon check

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-# write your code here
+#Python3.10
+
+
 def analyzer(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         line_num = 1
         for line in file:
+            code = line.split('#')[0].strip()
             if len(line) > 79:
                 print(f'Line {line_num}: S001 Too Long')
+            if line.startswith(' ' * 4):
+                print(f'Line {line_num}: S002 Indentation is not a multiple of four')
+            if code.endswith(';'):
+                print(f'Line {line_num}: S003 Unnecessary semicolon')
             line_num += 1
+            
 
 
 def main():
