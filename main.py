6a9b9f9b68c8d1a9ff0d675eafc574972b1f6cd4
Onur Surmeli <onatsurmeli@gmail.com>
2023-05-27 23:14:55 +0200

    added command line execution, added file and dir check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Python3.10
-
+import os
+import sys
 
 def analyzer(filename):
     blank_lines = 0
@@ -9,25 +10,25 @@ def analyzer(filename):
             code = line.split("#")[0][:-1]
             comment = line.split("#")[1:2]
             if len(line) > 79:
-                print(f"Line {line_num}: S001 Too Long")
+                print(f"{os.path.relpath(filename)}: Line {line_num}: S001 Too Long")
             if count_space(line) % 4 != 0:
-                print(f"Line {line_num}: S002 Indentation is not a multiple of four")
+                print(f"{os.path.relpath(filename)}: Line {line_num}: S002 Indentation is not a multiple of four")
             if code.strip().endswith(";"):
-                print(f"Line {line_num}: S003 Unnecessary semicolon")
+                print(f"{os.path.relpath(filename)}: Line {line_num}: S003 Unnecessary semicolon")
             if comment:
                 if code:
                     reversed_code = code[::-1]
                     if count_space(reversed_code) < 1:
                         print(
-                            f"Line {line_num}: S004 At least two spaces before inline comment required"
+                            f"{os.path.relpath(filename)}: Line {line_num}: S004 At least two spaces before inline comment required"
                         )
                 if "todo" in comment[0].lower():
-                    print(f"Line {line_num}: S005 TODO found")
+                    print(f"{os.path.relpath(filename)}: Line {line_num}: S005 TODO found")
             if code.strip() == "":
                 blank_lines += 1
             else:
                 if blank_lines > 2:
-                    print(f"Line {line_num}: S006 More than two blank lines used")
+                    print(f"{os.path.relpath(filename)}: Line {line_num}: S006 More than two blank lines used")
                 blank_lines = 0
             line_num += 1
 
@@ -43,9 +44,18 @@ def count_space(line):
 
 
 def main():
-    filename = input()
-    analyzer(filename)
+    args = sys.argv
+    file_or_folder(args[1])
 
 
-if __name__ == "__main__":
-    main()
+def file_or_folder(path):
+    if os.path.isdir(path):
+        filelist = sorted(os.listdir(path))
+        for file in filelist:
+            if file.endswith(".py"):
+                analyzer(os.path.join(path, file))
+    else:
+        analyzer(path)
+
+
+main()
